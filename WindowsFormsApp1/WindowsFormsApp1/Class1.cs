f3fb02d1a3fb00ceab09cdf48c73faec84801a55
TamaraMalyshkina <60821366+TamaraMalyshkina@users.noreply.github.com>
2020-02-08 21:11:52 +0500

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    class Class1
+    {
+        //как было показанно на видео
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Sourse = .\SQLEXPRESS; Initial Catalog = ShopDB; Intergrated Security = True";
+            SqlConnection connection = new SqlConnection(conStr);
+            try
+            {
+                connection.Open();
+                Console.WriteLine(connection.State);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine(connection.State);
+            }
+        }
+        // как меня обучили на работе
+        MySqlConnection connection = new MySqlConnection("serwer=localhost; port=3306; username=root; password=root; database=BD");
+
+        public void open()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void close()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection get()
+        {
+            return connection;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String login = textBox1.Text;
+            String pass = textBox2.Text;
+
+            Class1 class1 = new Class1();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM 'users' WHERE 'login' = @L AND 'pass' = @P", class1.get());
+            command.Parameters.Add("@L", MySqlDbType.VarChar).Value = login;
+            command.Parameters.Add("@P", MySqlDbType.VarChar).Value = pass;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+                MessageBox.Show("Молодец, хорошая память!");
+            else
+                MessageBox.Show("Ты чё-то попутал. Давай ещё. Напрягай свою единственную извилину!");
+        }
+    }
+}
